fe0b31eda77c21f4592dd581bcf4186ed8a1068b
Bruno Ferreira <brunoferreirabf2002@gmail.com>
2026-01-16 01:57:02 -0300

    atualização

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/api-tasks-frontend/', // ⬅️ nome do repositório
   plugins: [
     react(),
     tailwindcss(),
