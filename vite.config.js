584ccfd3d6be92e2ec8bdb61cb5fb467d1c8f9a6
Bruno Ferreira <brunoferreirabf2002@gmail.com>
2026-01-16 02:03:54 -0300

    fix: base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/api-tasks-frontend/', // ⬅️ nome do repositório
+  base: '/dailyTasks/',
   plugins: [
     react(),
     tailwindcss(),
